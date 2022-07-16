74407e6a248f6cb2335bee2b522e7442826142a3
okuxxbos20 <okuxxbos20@gmail.com>
2022-07-17 02:42:22 +0900

    ThemeProvider追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,38 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import { ThemeProvider } from '@mui/material/styles'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import PropTypes from 'prop-types'
+import createEmotionCache from '../createEmotionCache'
+import { theme } from '../theme'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
 }
 
-export default MyApp
+const App = ({
+  Component,
+  emotionCache = createEmotionCache(),
+  pageProps
+}: MyAppProps) => {
+  return (
+    <>
+      <Head>
+        <title>this is title</title>
+      </Head>
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    </>
+  )
+}
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired
+}
+
+export default App
