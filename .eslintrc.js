1bb331b6847a289d500f3d6837a2567a43f70e4d
okuxxbos20 <okuxxbos20@gmail.com>
2022-07-16 22:42:39 +0900

    jsonファイルはコメントが書けないのでjsファイルで定義

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['next/core-web-vitals', 'prettier'],
+  rules: {
+    'import/no-default-export': 'error',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc'
+        }
+      }
+    ]
+  },
+  overrides: [
+    // Next.jsのpagesではdefault exportで記述するため、rulesをoverrideする
+    {
+      files: ['*/pages/*'],
+      rules: {
+        'import/no-default-export': 'off',
+        'import/prefer-default-export': 'error'
+      }
+    }
+  ]
+}
