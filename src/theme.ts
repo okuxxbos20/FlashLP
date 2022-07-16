3b7df90e93da7c53710f34bfbec7274059724007
okuxxbos20 <okuxxbos20@gmail.com>
2022-07-17 02:38:33 +0900

    globalで使うthemeを設定

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2E3131'
+    },
+    secondary: {
+      main: '#6C7A89'
+    },
+    error: {
+      main: '#EB4747'
+    }
+  }
+})
